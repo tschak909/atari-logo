04f2614ae6fff311bf96b1b1b7664f63d48d4736
John Tsombakos <johnt519@gmail.com>
2021-04-22 11:13:50 -0400

    Fixed typo of DH address

--- src/atari-logo.s
+++ src/atari-logo.s
@@ -6,21 +6,21 @@ C1	=	$F1
 C2	=	$F2
 C3	=	$F3
 DL	=	$F4
-DH	=	$Ff5
-	
+DH	=	$F5
+
 SDLSTL	=	$0230
 SDLSTH	=	$0231
-	
-	
+
+
 	ORG	$2200
 
 START:
 	;; CLEAR KEYBOARD BUFFER
 	LDA	#$FF
 	STA	$02FC
-	
+
 	;; SAVE DISPLAY LIST
-	
+
 	LDA	SDLSTL
 	STA	DL
 	LDA	SDLSTH
@@ -41,7 +41,7 @@ SL1:	LDA	$02C4,X
 	STA	$02C5
 	STA	$02C6
 	STA	$02C7
-	
+
 	;; SET DISPLAY LIST
 
 	LDA	#$00
@@ -51,7 +51,7 @@ SL1:	LDA	$02C4,X
 
 	LDX	#$FF
 	STX	$02FC
-	
+
 	;; Main loop
 
 LOOP:	STX	$D016
@@ -59,7 +59,7 @@ LOOP:	STX	$D016
 	DEX
 	LDA	$02FC
 	BMI	LOOP
-	
+
 	;; RESTORE DISPLAY LIST
 
 	LDA	DL
@@ -74,7 +74,7 @@ SL2:	LDA	C0,X
 	STA	$02C4,X
 	DEX
 	BPL	SL2
-	
+
 	RTS
 
 	ORG	$2900
@@ -104,7 +104,7 @@ DLST:
 	.BYTE   $0E,$0E,$0E,$0E,$0E,$0E,$0E,$0E,$0E,$0E
 	.BYTE	$0E,$0E,$0E,$0E,$0E,$0E,$0E
 	.BYTE	$41,$00,$29
-	
+
 	ORG 	$3150
 
 	INS	"atari.mic"
